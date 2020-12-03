24a17966ff6cc1faba213d319fbb9a8ae438dd33
ErciliaR <72762687+ErciliaR@users.noreply.github.com>
2020-12-03 11:06:22 -0300

    Erci

---
+++ db_padaria.sql
@@ -0,0 +1,13 @@
+/*create database db_padaria;
+use db_padaria;*/
+
+/*create table tb_forma_pgm (
+id int(1) auto_increment,
+forma char(30) not null,
+debito boolean,
+primary key (id)
+);*/
+
+/*insert into tb_forma_pgm (forma, debito) values ("cartão", true);
+insert into tb_forma_pgm (forma, debito) values ("cartão", false);
+insert into tb_forma_pgm (forma, debito) values ("dinheiro", true);*/
