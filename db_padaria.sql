6ecac6f8d16a6ecaaa00dd74293543602e4115ed
Ercília R S Dantas <72762687+ErciliaR@users.noreply.github.com>
2020-12-03 13:49:29 -0300

    Atualizado e rodando

--- db_padaria.sql
+++ db_padaria.sql
@@ -17,7 +17,23 @@ insert into tb_tipo_pao (descricao,disponivel,preco) values ("Pão de forma",tru
 insert into tb_tipo_pao (descricao,disponivel,preco) values ("Pão grances",true,10.00);
 insert into tb_tipo_pao (descricao,disponivel,preco) values ("Pão caseiro",false,29.50); */
 
+create table tb_cliente(
+id bigint auto_increment,
+nome varchar (255),
+cpf char (11),
+celular char (12),
+endereco varchar (255),
+numero int,
+cep char(8),
 
+primary key (id)
+);
+
+insert into tb_cliente (nome, cpf, celular, endereco, numero, cep) values ("Caio", "11111111111", "19111111111", "Campinas", 1, "01111111");
+insert into tb_cliente (nome, cpf, celular, endereco, numero, cep) values ("Erci", "22222222222", "19222222222", "Campinas", 2, "02222222");
+insert into tb_cliente (nome, cpf, celular, endereco, numero, cep) values ("Leo", "33333333333", "193333333333", "Campinas", 3, "03333333");
+insert into tb_cliente (nome, cpf, celular, endereco, numero, cep) values ("Lukinhas", "44444444444", "194444444444", "Campinas", 4, "04444444");
+insert into tb_cliente (nome, cpf, celular, endereco, numero, cep) values ("Vitinho", "55555555555", "195555555555", "Campinas", 5, "05555555");
 
 
 create table tb_tipo_bolo(
@@ -42,7 +58,19 @@ insert into tb_tipo_bolo (descricao, disponivel, amanhecido, valor)
 insert into tb_tipo_bolo (descricao, disponivel, amanhecido, valor)
 	values ("bolo de festa", true, true, 14.99);
 
+CREATE TABLE bebidas(
+	id bigint(4) auto_increment,
+    descricao varchar(60),
+    disponivel boolean,
+    valor decimal(5,2),
+    primary key(id)
+);
 
+INSERT INTO bebidas (descricao, disponivel, valor) VALUES ("Delvalle 500ml", true, 5.00);
+INSERT INTO bebidas (descricao, disponivel, valor) VALUES ("Suco de laranja 500ml", true, 5.00);
+INSERT INTO bebidas (descricao, disponivel, valor) VALUES ("Coca-cola 500ml", true, 5.50);
+INSERT INTO bebidas (descricao, disponivel, valor) VALUES ("Guaraná 2l", true, 9.00);
+INSERT INTO bebidas (descricao, disponivel, valor) VALUES ("Tampico 2l", true, 7.00);
 
 use db_padaria;
 create table tb_forma_pgm (
@@ -79,8 +107,4 @@ values ("sua comanda foi ",1,1,1,1,1);
 use db_padaria;
 SELECT * FROM tb_comanda
 inner join tb_cliente
-on tb_comanda.id = tb_cliente.cliente_id; 
-
-/*inner join tb_cliente,tb_forma_pgm,tb_tipo_bolo,tb_tipo_pao
-on tb_comanda.id = tb_cliente.cliente_id,tb_forma_pgm.forma_pag_id,tb_tipo_bolo.tipo_bolo_id,tb_tipo_pao.tipo_pao_id;
-*/
+on tb_comanda.cliente_id = tb_cliente.id; 
